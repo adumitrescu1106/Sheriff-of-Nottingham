01a4da94ed03237a6158a4c6fa0aa2dfb7d97267
adumitrescu1106 <maxandyx13@gmail.com>
2019-11-01 11:53:14 +0200

    first

--- src/com/tema1/helpers/Constants.java
+++ src/com/tema1/helpers/Constants.java
@@ -4,4 +4,5 @@ public final class Constants {
     // add/delete any constants you think you may use
     public static final int CARDS_PICK = 10;
     public static final int START_BUDGET = 80;
+    public static final int LEGAL_BOUND = 9;
 }
--- src/com/tema1/main/Basic.java
+++ src/com/tema1/main/Basic.java
@@ -1,17 +1,24 @@
 package com.tema1.main;
 
+import com.tema1.goods.Goods;
+import com.tema1.goods.GoodsFactory;
+import com.tema1.helpers.Constants;
+
 import java.util.ArrayList;
 import java.util.List;
 
 
 public abstract class Basic extends Player {
-
+    private Constants constants;
+    private GoodsFactory products;
     public Basic() {
         super();
+        this.products = GoodsFactory.getInstance();
     }
 
     public Basic(List<Integer> cardsInHand, List<String> PlayerNames, int i) {
         super(cardsInHand, PlayerNames, i);
+        this.products = GoodsFactory.getInstance();
     }
 
     public final void playBasic(Player player) {
@@ -24,12 +31,33 @@ public abstract class Basic extends Player {
 
     public final void basicMerchant(Player player) {
         ArrayList<Integer> frequency = new ArrayList<Integer>();
-
+        if (!(checkIfLegal(getCardsInHand()))) {
+            getCardMaxProfit(getCardsInHand());
+        }
     }
 
     public void basicSheriff(Player player) {
 
     }
 
+    public final boolean checkIfLegal(List<Integer> cardsInHand) {
+        for (int i = 0; i < cardsInHand.size(); ++i) {
+            if (cardsInHand.get(i) <= constants.LEGAL_BOUND) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    public final void getCardMaxProfit(List<Integer> cardsInHand) {
+        int max = cardsInHand.get(0);
+        Goods aux = products.getGoodsById(max);
+        for (int i = 1; i < cardsInHand.size(); ++i) {
+            if (products.getGoodsById(cardsInHand.get(i)).getProfit() > aux.getProfit()) {
+                max = cardsInHand.get(i);
+            }
+        }
+
+    }
 
 }
